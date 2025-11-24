7b108d7f16d44d750291c75249d59398176e45a2
rezleader <brian@agentmannix.com>
2025-11-23 20:24:44 -0500

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // ADD THIS LINE BELOW:
-  base: '/Agent-Eddie-Sing-App/',
 })
